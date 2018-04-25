3f05f09eaf9b263e408f8be1980f5ab0f1c3cfa9
Theodore Stites <argionad@gmail.com>
2018-04-24 20:22:36 -0400

    Add fields, setters/getters to Receipt class

--- app/src/main/java/edu/ccm/tstites/personalexpenditures/CoreObjects/Receipt.java
+++ app/src/main/java/edu/ccm/tstites/personalexpenditures/CoreObjects/Receipt.java
@@ -1,9 +1,60 @@
 package edu.ccm.tstites.personalexpenditures.CoreObjects;
 
+import java.io.File;
+import java.util.Date;
+
 /**
  * Created by tstites on 4/21/2018.
  */
 
 public class Receipt {
+    private String mTitle;
+    private String mCategory;
+    private String mLocation;
+    private Date mDate;
+    private File mReceiptImage;
 
+    public Receipt() {
+        mDate = new Date();
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmCategory() {
+        return mCategory;
+    }
+
+    public void setmCategory(String mCategory) {
+        this.mCategory = mCategory;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public void setmLocation(String mLocation) {
+        this.mLocation = mLocation;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public File getmReceiptImage() {
+        return mReceiptImage;
+    }
+
+    public void setmReceiptImage(File mReceiptImage) {
+        this.mReceiptImage = mReceiptImage;
+    }
 }
