1d40ece05fc64644e602c8e72db7cddedc642743
Theodore Stites <argionad@gmail.com>
2018-04-26 13:42:55 -0400

    Initial setup of AddReceiptFragment

--- app/src/main/java/edu/ccm/tstites/personalexpenditures/fragments/AddReceiptFragment.java
+++ app/src/main/java/edu/ccm/tstites/personalexpenditures/fragments/AddReceiptFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import edu.ccm.tstites.personalexpenditures.R;
 
@@ -16,10 +19,32 @@ import edu.ccm.tstites.personalexpenditures.R;
 
 public class AddReceiptFragment extends Fragment {
 
+    private EditText mTitle;
+    private EditText mCategory;
+    private EditText mLocation;
+    private ImageView mReceiptImage;
+    private ImageButton mSaveButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_add, container, false);
+
+        mTitle = v.findViewById(R.id.edt_title);
+
+        mCategory = v.findViewById(R.id.edt_category);
+
+        mLocation = v.findViewById(R.id.edt_location);
+
+        mReceiptImage = v.findViewById(R.id.add_receipt_image);
+
+        mSaveButton = v.findViewById(R.id.btn_saveReceipt);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return v;
     }
